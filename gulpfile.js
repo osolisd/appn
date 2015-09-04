2f03d18b21e718dfe1d7afde6f16e09c766c7376
David Castillo <castillobgr@gmail.com>
2015-09-03 20:11:55 -0500

    Tests setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp    = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    Server = require('karma').Server;
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
 
 gulp.task('connect', function() {
   connect.server({
---
+++ spec/test.spec.js
@@ -0,0 +1,5 @@
+describe('An example test', function () {
+  it('passes', function(){
+    expect(2 + 2).toBe(4);
+  });
+});
