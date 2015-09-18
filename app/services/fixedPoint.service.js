86ff859d06e6e5ea02b8814b2faf32df51fce52c
Luis David Hurtado Cardona <ldh923@hotmail.com>
2015-09-18 00:54:54 -0500

    Service already complete, rest to do proves

--- app/services/fixedPoint.service.js
+++ app/services/fixedPoint.service.js
@@ -2,23 +2,24 @@ angular.module('numerical-analysis')
 .factory('FixedPoint', function(MethodCommons) {
   'use strict';
 
+  function calculate(fn, gn,varName,x0,nmax,tol,delta){
+    var i=0;
+    var x = x0;
+    var err = 1;
+    var data = {};
+    var q = x0;
+    var fx = MethodCommons.evaluate(fn,x0);
+    while (err > tol && i < nmax && fx > delta){
+      q = x;
+      x = MethodCommons.evaluate(gn,x);
+      fx = MethodCommons.evaluate(fn,x);
+      err = Math.abs(x-q);
+      data.push(angular.merge({fx : fx, gx : x, error : err}));
+    }
+    return data;
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return {
+    calculate: calculate
+  };
 });
