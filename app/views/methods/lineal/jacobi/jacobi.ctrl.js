557bcc0de08e72d7f4576b8819adc378d42960ac
David Castillo <castillobgr@gmail.com>
2015-10-12 18:25:56 -0500

    Jacobi view & controller

--- app/views/methods/lineal/jacobi/jacobi.ctrl.js
+++ app/views/methods/lineal/jacobi/jacobi.ctrl.js
@@ -1,19 +1,26 @@
 angular.module('numerical-analysis')
-.controller('JacobiCtrl', function (Jacobi) {
+.controller('JacobiCtrl', function (Jacobi, Utils) {
 
   var self = this;
-  self.file = null;
 
-  self.A = [
-    [2, 1],
-    [5, 7]
-  ];
+  self.matAStr = null;
+  self.vectBStr = null;
+  self.vectXStr = null;
 
-  self.b = [11, 13];
-
-  self.x = [0, 0];
+  self.options = {
+    nmax : 100,
+    delta : 0,
+    tol : 0
+  };
 
   self.calculate = function () {
-    self.res = Jacobi.calculate(self.A, self.b, self.x, 100, 0, 0);
+    var matA = Utils.parseCSV(self.matAStr, '\n', ';').map(function (row) {
+      return row.map(parseFloat);
+    });
+    var vectB = Utils.parseCSV(self.vectBStr, '\n', ';')[0].map(parseFloat);
+    var vectX = Utils.parseCSV(self.vectXStr, '\n', ';')[0].map(parseFloat);
+
+    self.res =
+      Jacobi.calculate(matA, vectB, vectX, self.options.nmax, self.options.delta, self.options.tol);
   };
 })
