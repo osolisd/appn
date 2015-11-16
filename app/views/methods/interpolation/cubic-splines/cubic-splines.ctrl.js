0ae2f93c9031b1fe17e2f98dc9b6ae8adb38cc97
David Castillo <castillobgr@gmail.com>
2015-11-16 13:28:47 -0500

    Cubic splines progress

--- app/views/methods/interpolation/cubic-splines/cubic-splines.ctrl.js
+++ app/views/methods/interpolation/cubic-splines/cubic-splines.ctrl.js
@@ -2,4 +2,24 @@ angular.module('numerical-analysis')
 .controller('CubicSplinesCtrl', function (CubicSplines, Utils) {
 
   var self = this;
+
+  self.results = [];
+
+  self.xs = [50, 200, 350];
+  self.ys = [50, 149.49363435889023, 249.12670270278602];
+
+  self.calculate = function () {
+    var res = CubicSplines.calculate(self.xs, self.ys);
+    for(var i = 0; i < res.length; i++) {
+      var result = {};
+      result.desc = 'From x = ' + self.xs[i] + ' to x = ' + self.xs[i + 1] + ':';
+      var splineStr = res[i].a;
+      splineStr += ' + ' + res[i].b + '(x - ' + res[i].x + ')';
+      splineStr += ' + ' + res[i].c + '(x - ' + res[i].x + ')^2';
+      splineStr += ' + ' + res[i].d + '(x - ' + res[i].x + ')^3';
+      result.spline = splineStr;
+      self.results.push(result);
+    }
+    console.log();
+  };
 });
