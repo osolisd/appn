2abb566be2675d7cec667bdd75a0bc6ebbc2e89e
Felipe Londono <pipes.20@hotmail.com>
2015-11-16 21:18:04 -0500

    Lineal splines and secant-sor fix

--- app/views/methods/non-lineal/secant/secant.ctrl.js
+++ app/views/methods/non-lineal/secant/secant.ctrl.js
@@ -7,7 +7,7 @@ angular.module('numerical-analysis')
   self.Calculate = function calculatebysecant(){
     Secant.CalculateBySecant(self.func,self.a,self.b,self.nmax,self.tol,self.delta);
     self.result = Secant.getResult();
-    self.reason = Secant.getReason();
+    //self.reason = Secant.getReason();
     self.arrx = Secant.getx();
     self.arrf = Secant.getfx();
     self.arrError = Secant.getError();
